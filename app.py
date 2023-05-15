c5cbb207163d5cff3d8a7f16006d318b53c1eca2
0xNaylor <0xnaylor@gmail.com>
2023-05-15 11:46:47 +0100

    Restyled table to improve readability

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ from views.fear_and_greed_index import display_fear_and_greed_chart
 from views.fed_liquidity_index import display_fed_liquidity_index_chart
 from views.annualized_btc_volatility_30d import display_btc_annualized_volatility_30d
 from views.bitcoin_volatility_index import display_btc_volatility_index
+from views.token_watchlist import display_token_watchlist
 
 server = app.server
 app.layout = html.Div(
@@ -70,6 +71,11 @@ app.layout = html.Div(
             dbc.Row(
                 [
                     dbc.Col(display_bit1_portfolio_table_usd(), width=8),
+                ],
+            ),
+            dbc.Row(
+                [
+                    dbc.Col(display_token_watchlist(), width=8),
                 ],
             ),
             dbc.Row(
